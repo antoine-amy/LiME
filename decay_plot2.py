0fe0491b1cebb2005f02436738bd8d8ea6f6d846
Antoine AMY <antoine.amy72@gmail.com>
2023-01-31 21:03:17 +0100

    good 2

--- decay_plot2.py
+++ decay_plot2.py
@@ -4,12 +4,12 @@ from scipy.optimize import curve_fit
 
 #Open files
 with open("data/run_20230126_bckgrd.txt", "r") as data:
-    data = [float(x)*10 for x in data.read().split() if float(x) > 1.395]
+    data = [float(x)*1 for x in data.read().split()]
 #22.11 pour 99% de muon désintegre
 mean_coincid=0
 
 #create the histogram and the error bars
-n, bins, patches = plt.hist(data, bins=9, edgecolor='black', density=True)
+n, bins, patches = plt.hist(data, bins=18, edgecolor='black', density=True)
 n=n-mean_coincid
 bin_centers = (bins[:-1] + bins[1:]) / 2
 #To start at the second bin
@@ -23,4 +23,13 @@ num_values = len(data)
 plt.annotate(f"Number of values: {num_values}", xy=(0.05, 0.95), xycoords='axes fraction',
              fontsize=14, color='black', horizontalalignment='left', verticalalignment='top')
 plt.legend()
+
+sum=0
+for x in range (2,17):
+    sum=n[x]+sum
+
+mean=sum/16
+
+print(n)
+print(mean/10)
 plt.show()
